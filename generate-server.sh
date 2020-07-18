24cb2a95ee6100b7062bfbb792b7afc485d56bda
Maximiliano Osorio <maxiosorio@gmail.com>
2020-07-18 15:41:02 -0700

    add server

---
+++ generate-server.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+dir=${PWD}
+SERVER_DIR=server
+docker run -ti --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v4.3.1 \
+     generate  \
+     -i /local/openapi.yaml\
+     -g go-server  \
+     -o /local/ \
+     --git-repo-id jwt_example \
+     --git-user-id mosoriob \
