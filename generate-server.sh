3c7f24d67a4d89398d216478caa7b0c6d731d45d
Maximiliano Osorio <maxiosorio@gmail.com>
2020-07-18 15:43:23 -0700

    fix: clean

--- generate-server.sh
+++ generate-server.sh
@@ -5,7 +5,7 @@ SERVER_DIR=server
 docker run -ti --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v4.3.1 \
      generate  \
      -i /local/openapi.yaml\
-     -g go-server  \
+     -g go-gin-server  \
      -o /local/ \
      --git-repo-id jwt_example \
      --git-user-id mosoriob \
--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/mosoriob/jwt_example
-
-go 1.13
-
-require github.com/gorilla/mux v1.7.3
